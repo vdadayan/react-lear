f53c654d7c512f6421db229ab290fdab6a772779
Vitaly Dadayan <vitaliyches@rambler.ru>
2021-06-10 22:46:19 +0300

    81 урок селекторы 1 часть

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -4,6 +4,7 @@ import { follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers } f
 import Users from './Users';
 import Preloader from '../Preloader/Preloader';
 import { compose } from 'redux';
+import { getPageSize, getTotalUsersCount, getUsersSelector, getCurrentPage, getIsFetching, getFollowingInProgress } from '../../Redux/users-selectors';
 
 
 class UsersAPIComponent extends React.Component {
@@ -31,17 +32,27 @@ class UsersAPIComponent extends React.Component {
     }
 }
 
+// let mapStateToProps = (state) => {
+//     return {
+//         users: state.usersPage.users,
+//         pageSize: state.usersPage.pageSize,
+//         totalUsersCount: state.usersPage.totalUsersCount,
+//         currentPage: state.usersPage.currentPage,
+//         isFetching: state.usersPage.isFetching,
+//         followingInProgress: state.usersPage.followingInProgress,
+//     }
+// }
+
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress,
+        users: getUsersSelector(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state),
     }
 }
-
 export default compose(
     connect(mapStateToProps, 
         {follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers})
