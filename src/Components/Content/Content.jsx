0e318268f273f2f247296126bcd1804b1d182965
Vitaliy Dadayan <dadayan@e-krit.ru>
2021-06-28 16:53:12 +0300

    94 lazy Suspense

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { NavLink, Route } from 'react-router-dom';
-import DialogsContainer from './Dialogs/DialogsContainer';
+import {NavLink, Route} from 'react-router-dom';
+// import DialogsContainer from './Dialogs/DialogsContainer';
 import UsersContainer from '../Users/UsersContainer';
-import ProfileContainer from './Profile/ProfileContainer';
+// import ProfileContainer from './Profile/ProfileContainer';
 import './Content.scss';
 import Login from './Login/Login';
-import { connect } from 'react-redux';
-import { initialize } from '../../Redux/reducers/appReducer';
-import { toAuth } from '../../Redux/reducers/authReducer';
+import {connect} from 'react-redux';
+import {initialize} from '../../Redux/reducers/appReducer';
+import {toAuth} from '../../Redux/reducers/authReducer';
 import Preloader from '../Preloader/Preloader';
+import {SuspenseHOC} from "../../Hoc/SuspenseHOC";
+
+//lazy
+const DialogsContainer = React.lazy(() => import('./Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./Profile/ProfileContainer'));
+
 
 class Content extends React.Component {
     componentDidMount() {
@@ -19,29 +25,30 @@ class Content extends React.Component {
         if (!this.props.initialized) {
             return <Preloader/>
         }
-        return(
-                <div className="content">
-                    <nav>
-                        <ul>
-                            <li><NavLink to="/Profile">Profile</NavLink></li>
-                            <li><NavLink to="/Dialogs">Messages</NavLink></li>
-                            <li><NavLink to="/Users">Users</NavLink></li>
-                            <li><a href="/#">News</a></li>
-                            <li><a href="/#">Music</a></li>
-                            <li><a href="/#">Settings</a></li>
-                        </ul>
-                    </nav>
-                    <Route path="/Profile/:userId?" render={() => <ProfileContainer/>}/>
-                    <Route path="/Dialogs" render={() => <DialogsContainer />}/>
-                    <Route path="/Users" render={() => <UsersContainer />}/>
-                    <Route path="/Login" render={() => <Login />}/>
-                </div> 
+        return (
+            <div className="content">
+                <nav>
+                    <ul>
+                        <li><NavLink to="/Profile">Profile</NavLink></li>
+                        <li><NavLink to="/Dialogs">Messages</NavLink></li>
+                        <li><NavLink to="/Users">Users</NavLink></li>
+                        <li><a href="/#">News</a></li>
+                        <li><a href="/#">Music</a></li>
+                        <li><a href="/#">Settings</a></li>
+                    </ul>
+                </nav>
+                <Route path="/Profile/:userId?" render={SuspenseHOC(ProfileContainer)}
+                />
+                <Route path="/Dialogs" render={SuspenseHOC(DialogsContainer)}/>
+                <Route path="/Users" render={() => <UsersContainer/>}/>
+                <Route path="/Login" render={SuspenseHOC(Login)}/>
+            </div>
         )
     }
 }
 
 const mapStateToProps = (state) => {
     return {initialized: state.app.initialized}
-} 
+}
 
-export default connect(mapStateToProps,{initialize,toAuth})(Content);
+export default connect(mapStateToProps, {initialize, toAuth})(Content);
